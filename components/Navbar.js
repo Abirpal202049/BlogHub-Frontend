0b69c829bacc65932abe549c2fc2a21f526a9c0b
Abir Pal <abirpal282002@gmail.com>
2022-07-10 11:33:12 +0530

    Blog page done

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
@@ -6,18 +6,36 @@ export default function Navbar() {
       <header className="text-gray-300 body-font bg-slate-900">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a className="flex title-font font-extralight items-center text-gray-500 mb-4 md:mb-0">
-            
-            <img src="https://res.cloudinary.com/duavagcwx/image/upload/v1657389034/22520079_3c4dd1719a.jpg?updated_at=2022-07-09T17:50:35.316Z" className="w-10 h-10 rounded-full scale-125" alt="" />
+            <img
+              src="https://res.cloudinary.com/duavagcwx/image/upload/v1657389034/22520079_3c4dd1719a.jpg?updated_at=2022-07-09T17:50:35.316Z"
+              className="w-10 h-10 rounded-full scale-125"
+              alt=""
+            />
             <span className="ml-3 text-2xl">TargetTechnology</span>
           </a>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-gray-100">Home</a>
-            <a className="mr-5 hover:text-gray-100">About</a>
-            <a className="mr-5 hover:text-gray-100">Works</a>
-            <a className="mr-5 hover:text-gray-100">Blogs</a>
-            <a className="mr-5 hover:text-gray-100">Conatct</a>
+
+            <Link href="/">
+              <a className="mr-5 hover:text-gray-100">Home</a>
+            </Link>
+
+            <Link href="/about">
+              <a className="mr-5 hover:text-gray-100">About</a>
+            </Link>
+
+            <Link href="/work">
+              <a className="mr-5 hover:text-gray-100">Works</a>
+            </Link>
+
+            <Link href="/blogs">
+              <a className="mr-5 hover:text-gray-100">Blogs</a>
+            </Link>
+
+            <Link href="/contact">
+              <a className="mr-5 hover:text-gray-100">Conatct</a>
+            </Link>
+            
           </nav>
-          
         </div>
       </header>
     </>
---
+++ pages/blogs/index.js
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdDateRange } from "react-icons/md";
+
+export async function getStaticProps(context) {
+  const res = await fetch(
+    `https://bloghubbackend.herokuapp.com/api/blog-posts?populate=*`
+  );
+  const { data } = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default function Blogs({ data }) {
+  console.log(data);
+  return (
+    <>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap -m-4">
+            {data.map((blog, index) => {
+              const { Heading, SubHeading, blog_catagory, slug, createdAt } =
+                blog.attributes;
+              const blogcatagory =
+                blog_catagory?.data?.attributes?.CatagoryName;
+              const catagorySlug =
+                blog_catagory?.data?.attributes?.catagoryslug;
+              return (
+                <div className="p-4 lg:w-1/3" key={index}>
+                  <div
+                    className={`h-full bg-gray-300 bg-cover bg-opacity-100 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative`}
+                  >
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-600 mb-1">
+                      <Link href={`/blogs/catagory/${catagorySlug}`}>
+                        <a>{blogcatagory}</a>
+                      </Link>
+                    </h2>
+                    <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                      {Heading}
+                    </h1>
+                    <p className="leading-relaxed mb-3">{SubHeading}</p>
+                    <Link href={`/blogs/${slug}`}>
+                      <a className="text-indigo-500 inline-flex items-center">
+                        Read More
+                      </a>
+                    </Link>
+                    <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                      <span class="text-gray-700 gap-2 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        <div class="border border-gray-700 p-1 flex justify-center items-center bg-slate-400 rounded-full">
+                          <AiOutlineUser />
+                        </div>
+                        Krishnendu Das
+                      </span>
+
+                      <span class="text-gray-700 gap-1 inline-flex items-center leading-none text-sm">
+                        <div>
+                          <MdDateRange size={20} />
+                        </div>
+                        {createdAt}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
